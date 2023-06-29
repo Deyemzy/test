7d3212f2d63e64b84e2474fc224893fb8894be29
Deyemzy <56632471+Deyemzy@users.noreply.github.com>
2023-06-28 21:49:28 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,52 +1,170 @@
-resource "aws_lambda_function" "backup_lambda" {
-  function_name    = "opensearch_backup"
-  handler          = "backup_lambda_function.lambda_handler"
-  runtime          = "python3.8"
-  filename         = "${path.module}/backup_lambda_function.zip"
-  source_code_hash = filebase64sha256("${path.module}/backup_lambda_function.zip")
-  role             = aws_iam_role.backup_lambda_role.arn
-}
+To implement the backup solution in the `atlas-opensearch-service` repository, you can follow these steps:
 
-resource "aws_iam_role" "backup_lambda_role" {
-  name = "opensearch_backup_lambda_role"
+1. Create a `backup` directory inside the `opensearch` directory of the repository:
 
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "lambda.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
-EOF
-}
+   ```
+   [opensearch]
+       ├── backend.tf
+       ├── CODEOWNERS
+       ├── domains.tf
+       ├── locals.tf
+       ├── main.tf
+       ├── outputs.tf
+       ├── providers.tf
+       ├── service-role.tf
+       ├── variables.tf
+       └── [backup]  <-- Create this directory
+   ```
 
-resource "aws_iam_role_policy_attachment" "backup_lambda_policy_attachment" {
-  role       = aws_iam_role.backup_lambda_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonESFullAccess"
-}
+2. Inside the `backup` directory, add the necessary files:
 
-resource "aws_lambda_permission" "backup_lambda_permission" {
-  statement_id  = "AllowExecutionFromCloudWatch"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.backup_lambda.arn
-  principal     = "events.amazonaws.com"
-  source_arn    = aws_cloudwatch_event_rule.backup_event_rule.arn
-}
+   - `lambda_function.py`: This is the backup Lambda function script.
+   - `requirements.txt`: This file should contain the required Python dependencies for the Lambda function.
 
-resource "aws_cloudwatch_event_rule" "backup_event_rule" {
-  name        = "opensearch_backup_event_rule"
-  description = "Schedule OpenSearch backup"
-  schedule_expression = "rate(1 day)"
-}
+   The directory structure will look like this:
 
-resource "aws_cloudwatch_event_target" "backup_event_target" {
-  rule      = aws_cloudwatch_event_rule.backup_event_rule.name
-  arn       = aws_lambda_function.backup_lambda.arn
-  target_id = "opensearch_backup_target"
-}
+   ```
+   [opensearch]
+       ├── backend.tf
+       ├── CODEOWNERS
+       ├── domains.tf
+       ├── locals.tf
+       ├── main.tf
+       ├── outputs.tf
+       ├── providers.tf
+       ├── service-role.tf
+       ├── variables.tf
+       └── [backup]
+           ├── lambda_function.py
+           └── requirements.txt
+   ```
+
+3. Modify the `main.tf` file in the `opensearch` directory to include the backup functionality:
+
+   ```terraform
+   module "opensearch_backup" {
+     source = "./backup"
+
+     # Pass necessary input variables to the backup module
+     bucket_name   = var.bucket_name
+     retention_period = var.retention_period
+
+     # Use data from YAML file for environment configuration
+     environments = var.environments
+   }
+   ```
+
+   The `main.tf` file will reference the `backup` module and pass the necessary input variables.
+
+4. Create the `variables.tf` file in the `opensearch` directory to define the input variables:
+
+   ```terraform
+   variable "bucket_name" {
+     type        = string
+     description = "The name of the S3 bucket for backup storage"
+   }
+
+   variable "retention_period" {
+     type        = number
+     description = "The retention period for backup snapshots in days"
+   }
+
+   variable "environments" {
+     type = list(object({
+       name                 = string
+       storage_volume_size  = number
+       instance_type        = string
+       instance_count       = number
+       tags                 = list(string)
+       user_vault_path      = string
+       public_facing        = bool
+       backup               = bool
+       domain_name          = string
+     }))
+     description = "The list of environments and their configurations"
+   }
+   ```
+
+   The `variables.tf` file defines the input variables needed for the backup module, including the S3 bucket name, retention period, and the list of environments with their configurations.
+
+5. Create the `backup.tf` file in the `backup` directory to define the backup module:
+
+   ```terraform
+   resource "null_resource" "lambda_function" {
+     provisioner "local-exec" {
+       command = "pip install -r ${path.module}/requirements.txt"
+     }
+   }
+
+   data "template_file" "lambda_function" {
+     template = file("${path.module}/lambda_function.py")
+
+     vars = {
+       environments = jsonencode(var.environments)
+     }
+   }
+
+   resource "aws_lambda_function" "backup_lambda" {
+     filename      = data.archive_file.lambda_function_zip.output_path
+     function_name = "opensearch_backup_lambda"
+     role          = aws_iam_role.lambda_execution_role.arn
+     handler       = "lambda_function.lambda_handler"
+     runtime       = "python3.9"
+
+     source_code_hash = data.archive_file.lambda_function_zip.output
+
+_base64sha256
+
+     environment {
+       variables = {
+         YAML_CONFIG = data.template_file.lambda_function.rendered
+       }
+     }
+   }
+
+   data "archive_file" "lambda_function_zip" {
+     type        = "zip"
+     output_path = "${path.module}/lambda_function.zip"
+     source {
+       content  = data.template_file.lambda_function.rendered
+       filename = "lambda_function.py"
+     }
+   }
+
+   resource "aws_iam_role" "lambda_execution_role" {
+     name = "opensearch_backup_lambda_role"
+
+     assume_role_policy = jsonencode({
+       "Version": "2012-10-17",
+       "Statement": [
+         {
+           "Sid": "",
+           "Effect": "Allow",
+           "Principal": {
+             "Service": "lambda.amazonaws.com"
+           },
+           "Action": "sts:AssumeRole"
+         }
+       ]
+     })
+
+     managed_policy_arns = [
+       "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+     ]
+   }
+   ```
+
+   The `backup.tf` file contains the Terraform configuration for the backup module, including the Lambda function resource, IAM role, and necessary data sources.
+
+6. Update the `outputs.tf` file in the `opensearch` directory to include any required output values from the backup module.
+
+   ```terraform
+   output "backup_lambda_function_name" {
+     value       = module.opensearch_backup.backup_lambda_function_name
+     description = "The name of the backup Lambda function"
+   }
+   ```
+
+   This example shows how to output the name of the backup Lambda function.
+
+With these configuration files, the backup functionality will be incorporated into the `atlas-opensearch-service` repository. The Lambda function and its requirements will be located in the `backup` directory, and the Terraform configuration files will handle the execution role and references to the YAML files.
