59a36dec3ab2ae521d132a5a8d50955d82aff7aa
Deyemzy <56632471+Deyemzy@users.noreply.github.com>
2023-07-18 19:39:55 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,22 +3,30 @@ resource "aws_opensearch_domain_policy" "main" {
   domain_name = aws_opensearch_domain.os.domain_name
 
   access_policies = jsonencode({
-    "Version": "2012-10-17",
-    "Statement": [
+    Version   = "2012-10-17"
+    Statement = [
       {
-        "Action": [
-          "es:*",
-          "logs:CreateLogStream"
-        ],
-        "Principal": "*",
-        "Effect": "Allow",
-        "Resource": "${aws_opensearch_domain.os.arn}/*",
-        "Condition": {
-          "StringLike": {
-            "aws:ResourceTag/${aws_opensearch_domain.os.tags.Name}": "*"
-          }
+        Action    = "es:*"
+        Principal = "*"
+        Effect    = "Allow"
+        Resource  = "${aws_opensearch_domain.os.arn}/*"
+      },
+      {
+        Action    = "logs:CreateLogStream"
+        Effect    = "Allow"
+        Principal = {
+          Service = "es.amazonaws.com"
         }
-      }
+        Resource  = "${module.logging["SEARCH_SLOW_LOGS"].log_group_arn}:*"
+      },
+      {
+        Action    = "logs:PutLogEvents"
+        Effect    = "Allow"
+        Principal = {
+          Service = "es.amazonaws.com"
+        }
+        Resource  = "${module.logging["SEARCH_SLOW_LOGS"].log_group_arn}:*"
+      },
     ]
   })
 }
