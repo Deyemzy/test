23f503ed0ca82d259699b1eaabb71c323d29db69
Deyemzy <56632471+Deyemzy@users.noreply.github.com>
2023-08-14 17:32:36 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,25 +1,6 @@
-n  AWSTemplateFormatVersion: "2010-09-09"
-Description: An IAM role template for StackSet deployment
-
-Resources:
-  CrossAccountRole:
-    Type: "AWS::IAM::Role"
-    Properties:
-      AssumeRolePolicyDocument:
-        Version: "2012-10-17"
-        Statement:
-          - Effect: "Allow"
-            Principal:
-              AWS:
-                - "<Management_Account_ID>"
-            Action:
-              - "sts:AssumeRole"
-      Path: "/"
-      RoleName: "MyCrossAccountRole"
-
-
-
-
-
-
-Created a StackSet in the management account to centrally manage and deploy IAM roles across multiple linked AWS accounts. This StackSet includes a CloudFormation template defining the desired IAM roles, policies, and trust relationships. By deploying this StackSet, we can ensure consistent IAM role configurations in each linked account, enhancing security and operational efficiency.
+resource "aws_cloudwatch_event_rule" "eventbridge_rule" {
+  count        = length(var.linked_account_ids)
+  name         = "EventBridgeRule-${var.linked_account_ids[count.index]}"
+  event_pattern = jsonencode({ "source": ["aws.config"], "detail-type": ["AWS API Call via CloudTrail"] })
+  # ...
+}
