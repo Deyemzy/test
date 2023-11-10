398f876260a6a5bdc1f72ca0ac761a5a6607a916
Deyemzy <56632471+Deyemzy@users.noreply.github.com>
2023-11-09 20:41:51 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,3 +18,19 @@ pip install requests -t .
 cd ..
 zip -r requests_layer.zip python
 aws lambda publish-layer-version --layer-name requests-layer --zip-file fileb://requests_layer.zip --compatible-runtimes python3.8
+
+
+import requests
+import urllib3
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+def lambda_handler(event, context):
+    logger.info(f"Requests version: {requests.__version__}")
+    logger.info(f"urllib3 version: {urllib3.__version__}")
+    return {
+        'statusCode': 200,
+        'body': 'Version check executed successfully!'
+    }
